9f62a8e3f99b99a5870759a68840b3cc026cca32
Thomas Remblier <thomas.rembl@gmail.com>
2024-09-27 15:49:04 +0200

    update

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,6 +1,5 @@
 //layout.tsx
 import Footer from "@/components/footer";
-import SmoothScrolling from "@/components/smoothScoll";
 import "@/styles/index.scss";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { useTranslations } from "next-intl";
